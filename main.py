7919f07072cef73d50d0c8795ad28c936daf8065
Wallace Baleroni <wbaleroni@muxi.corp>
2019-04-01 15:06:43 -0600

    Agora tem circulo e a linha da reta nao ta picotada

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 class Jogo:
@@ -17,6 +18,7 @@ class Jogo:
 
             # Desenha pixel
             self.reta([100, 100], [200, 345], [255, 255, 255])
+            self.circulo([100, 100], 100, [255, 255, 255])
 
             self.tela.blit(self.superficie, [0, 0])
             pygame.display.flip()
@@ -36,6 +38,30 @@ class Jogo:
 
             self.superficie.set_at([x, int(y)], cor)
 
+        for y in range(yi, yf + 1):
+        	m = (xf - xi) / (yf - yi)
+        	x = m * (y - yi) + xi
+
+        	self.superficie.set_at([int(x), y], cor)
+
+    def circulo(self, centro, raio, cor):
+    	a, b = centro
+
+    	for x in range(a - raio, a + raio):
+    		yroot = math.sqrt(math.pow(raio, 2) - math.pow(x - a, 2))
+    		y1 = yroot + b
+    		y2 = -yroot + b
+
+    		self.superficie.set_at([x, int(y1)], cor)
+    		self.superficie.set_at([x, int(y2)], cor)
+
+    	for y in range(b - raio, b + raio):
+    		xroot = math.sqrt(math.pow(raio, 2) - math.pow(y - b, 2))
+    		x1 = xroot + a
+    		x2 = -xroot + a
+
+    		self.superficie.set_at([int(x1), y], cor)
+    		self.superficie.set_at([int(x2), y], cor)
 
 jogo = Jogo()
 jogo.loop()
