da6f315f5a31d7e7445b0af7ad99ebb82d438c94
Júlia Falcão <julia.falcao@accenture.com>
2019-06-13 10:51:46 -0300

    alô quatérnios

---
+++ quaternios.py
@@ -0,0 +1,47 @@
+from math import *
+
+def produto_escalar(v1, v2):
+    assert len(v1) == len(v2)
+    
+    soma = 0
+    for i in range(len(v1)):
+        soma += v1[i] * v2[i]
+    
+    return soma
+
+def modulo(v):
+    soma = 0
+    for i in v:
+        soma += i**2
+    
+    return sqrt(soma)
+
+def produto_vetorial(a, b):
+    i = a[1]*b[2]-a[2]*b[1]
+    j = a[2]*b[0]-a[0]*b[2]
+    k = a[0]*b[1]-a[1]*b[0]
+    return (i, j, k)
+
+# dot = produto_escalar
+# x = produto_vetorial
+# * = conjugado
+
+def produto(x, v=(0, 0, 0)):
+    return [x * vi for vi in v]
+
+def conjugado(q):
+    return (q[0], -q[1], -q[2], -q[3])
+
+def rotacao(p, teta, n = (0, 0, 0)):
+    assert len(p) == 3
+    
+    r = p
+    p = (0, p[0], p[1], p[2])
+    s = cos(radians(teta/2))
+    v = [sen(radians(teta/2)) * ni for ni in n]
+    
+    qpq = produto(s**2, r) - (produto(produto_escalar(v, v), r)) + 
+    produto(2 * produto_escalar(v, r), v) +
+    produto(2 * s, produto_vetorial(v, r))
+    
+    return (0, qpq[0], qpq[1], qpq[2])
